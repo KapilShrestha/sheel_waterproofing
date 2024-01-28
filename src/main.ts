6c8c240fcac17b5640c608d23f4ab14b4a498759
Kapil Shrestha <kapilshrestha1001@gmail.com>
2024-01-28 23:38:30 +0545

    fix: scroll to about services contact

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,23 @@
-// src/main.ts
-
+// sheel_waterproofing/src/main.ts
 
 document.addEventListener('DOMContentLoaded', function () {
+  // dropdown in hover
+  const detailsElements = document.querySelectorAll('details');
+  detailsElements.forEach((details) => {
+    details.addEventListener('mouseover', function () {
+      details.setAttribute('open', 'true');
+    });
+    details.addEventListener('focus', function () {
+      details.setAttribute('open', 'true');
+    });
+    details.addEventListener('mouseout', function () {
+      details.removeAttribute('open');
+    });
+    details.addEventListener('blur', function () {
+      details.removeAttribute('open');
+    });
+  });
+
   // Function to fetch and insert content from a page
   function fetchAndInsertContent(page: string) {
     fetch(page)
@@ -33,8 +49,99 @@ document.addEventListener('DOMContentLoaded', function () {
     item.addEventListener('click', handleNavbarItemClick(page));
   });
 
+
+   // Function to scroll to the About section
+   function scrollToAbout() {
+    
+    const aboutSection = document.getElementById('about-section');
+    if (aboutSection) {
+      aboutSection.scrollIntoView({ behavior: 'smooth' });
+    } else {
+      console.error('About section not found.');
+    }
+  }
+
+  // Attach click event listener to the About navbar item
+  const aboutLink = document.getElementById('about-link');
+  const aboutLinkSm = document.getElementById('about-link-sm');
+  if (aboutLink) {
+    aboutLink.addEventListener('click', () => {
+      scrollToAbout();
+    });
+  } else {
+    console.error('About link not found.');
+  }
+  if (aboutLinkSm) {
+    aboutLinkSm.addEventListener('click', () => {
+      scrollToAbout();
+    });
+  } else {
+    console.error('About link not found.');
+  }
+
+   // Function to scroll to the Services section
+   function scrollToServices() {
+    const servicesSection = document.getElementById('services-section');
+    if (servicesSection) {
+      servicesSection.scrollIntoView({ behavior: 'smooth' });
+    } else {
+      console.error('Services section not found.');
+    }
+  }
+
+  // Attach click event listener to the Services navbar item
+  const servicesLink = document.getElementById('services-link');
+  const servicesLinkSm = document.getElementById('services-link-sm');
+  if (servicesLink) {
+    servicesLink.addEventListener('click', () => {
+      scrollToServices();
+    });
+  } else {
+    console.error('Services link not found.');
+  }
+  if (servicesLinkSm) {
+    servicesLinkSm.addEventListener('click', () => {
+      scrollToServices();
+    });
+  } else {  
+    console.error('Services link not found.');
+  }
+
+   // Function to scroll to the Contact section
+   function scrollToContact() {
+    const contactSection = document.getElementById('contact-section');
+    if (contactSection) {
+      contactSection.scrollIntoView({ behavior: 'smooth' });
+    } else {
+      console.error('Contact section not found.');
+    }
+  }
+
+  // Attach click event listener to the Contact navbar item
+  const contactLink = document.getElementById('contact-link');
+  const contactLinkSm = document.getElementById('contact-link-sm');
+  if (contactLink) {
+    contactLink.addEventListener('click', () => {
+      scrollToContact();
+    });
+  } else {
+    console.error('Contact link not found.');
+  }
+  if (contactLinkSm) {
+    contactLinkSm.addEventListener('click', () => {
+      scrollToContact();
+    });
+  }else {
+    console.error('Contact link not found.');
+  }
+
+
+
+
+ 
+
+
   // Load default content on page load
-  fetchAndInsertContent('../pages/vista_power_silica_fume.html');
+  fetchAndInsertContent('../pages/index_content.html');
 
 });
-
