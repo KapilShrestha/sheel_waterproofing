b2c6a9d43b11db74fcf068d14110320b6f683897
Kapil Shrestha <kapilshrestha1001@gmail.com>
2024-02-02 16:07:56 +0545

    fix: logo

--- src/main.ts
+++ src/main.ts
@@ -201,7 +201,7 @@ document.addEventListener('DOMContentLoaded', function () {
     console.error('Contact link not found.');
   }
   // Load default content on page load
-  fetchAndInsertContent('../pages/products.html');
+  fetchAndInsertContent('../pages/vista_superplast_hs_1000.html');
 
 
 });
