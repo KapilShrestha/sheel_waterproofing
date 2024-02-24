9dca0ee7449c7b74528ee16885d5095b1eeb57d9
Kapil Shrestha <kapilshrestha1001@gmail.com>
2024-02-24 16:06:17 +0545

    Add: new dist folder created with added pages

--- vite.config.ts
+++ vite.config.ts
@@ -9,18 +9,27 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'), // Entry point of your application
+                "bituminous_waterproofing_membrane": resolve(__dirname, "./pages/bituminous_waterproofing_membrane.html"),
+                "cementitious_waterproofing": resolve(__dirname, "./pages/cementitious_waterproofing.html"),
                 "index_content": resolve(__dirname, "./pages/index_content.html"),
+                "injection_waterproofing": resolve(__dirname, "./pages/injection_waterproofing.html"),
+                "liquid_waterproofing_membrane": resolve(__dirname, "./pages/liquid_waterproofing_membrane.html"),
+                "polyurethane_waterproofing": resolve(__dirname, "./pages/polyurethane_waterproofing.html"),
                 "products": resolve(__dirname, "./pages/products.html"),
+                "roof_waterproofing": resolve(__dirname, "./pages/roof_waterproofing.html"),
+                "vista_afa_super": resolve(__dirname, "./pages/vista_afa_super.html"),
+                "vista_crystal_plus": resolve(__dirname, "./pages/vista_crystal_plus.html"),
+                "vista_flex_seal_plus": resolve(__dirname, "./pages/vista_flex_seal_plus.html"),
                 "vista_iwa": resolve(__dirname, "./pages/vista_iwa.html"),
                 "vista_no_2": resolve(__dirname, "./pages/vista_no_2.html"),
                 "vista_no_3": resolve(__dirname, "./pages/vista_no_3.html"),
-                "vista_sbr_latex_plus": resolve(__dirname, "./pages/vista_sbr_latex_plus.html"),
-                "vista_super_crete": resolve(__dirname, "./pages/vista_super_crete.html"),
-                "vista_flex_seal_plus": resolve(__dirname, "./pages/vista_flex_seal_plus.html"),
                 "vista_power_silica_fume": resolve(__dirname, "./pages/vista_power_silica_fume.html"),
+                "vista_sbr_latex_plus": resolve(__dirname, "./pages/vista_sbr_latex_plus.html"),
+                "vista_shot_x": resolve(__dirname, "./pages/vista_shot_x.html"),
+                "vista_super_crete": resolve(__dirname, "./pages/vista_super_crete.html"),
                 "vista_superplast_hs": resolve(__dirname, "./pages/vista_superplast_hs.html"),
                 "vista_superplast_hs_1000": resolve(__dirname, "./pages/vista_superplast_hs_1000.html"),
-                "vista_afa_super": resolve(__dirname, "./pages/vista_afa_super.html"),
+                
             },
         },
         emptyOutDir: true, // Clear the output directory before building
